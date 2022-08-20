e7054972cabe859b49823e633a40a9a6fe48f856
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2022-08-20 20:18:13 +0530

    testing

--- infra/scripts/run_pytest_cdk.py
+++ infra/scripts/run_pytest_cdk.py
@@ -9,18 +9,18 @@ from script_utils import get_path_for_directory
 def main() -> None:
     """Invoke 'pytest' shell command."""
     
-    covreport = "AWS-Infrastructure-Coverage-Report.xml"
-    junitxml = "AWS-Infrastructure-Test-Result.xml"
+    # covreport = "AWS-Infrastructure-Coverage-Report.xml"
+    # junitxml = "AWS-Infrastructure-Test-Result.xml"
     os.chdir(get_path_for_directory("test_cdk_unit"))
     pytest.main()
     subprocess.check_output(
         "pip install pytest pytest-cov", shell=True, stderr=subprocess.STDOUT
     )
-    subprocess.check_output(
-        f"python -m pytest --cov --cov-report=xml:{covreport} --junitxml={junitxml}",
-        shell=True,
-        stderr=subprocess.STDOUT
-    )
+    # subprocess.check_output(
+    #     f"python -m pytest --cov --cov-report=xml:{covreport} --junitxml={junitxml}",
+    #     shell=True,
+    #     stderr=subprocess.STDOUT
+    # )
 
 
 if __name__ == "__main__":

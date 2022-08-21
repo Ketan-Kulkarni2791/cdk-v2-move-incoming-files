145eacabd4db420a3f2ffc1451a3e4854bf592cb
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2022-08-21 15:43:09 +0530

    minors

--- src/moving_incoming_files/main.py
+++ src/moving_incoming_files/main.py
@@ -6,10 +6,10 @@ Returns-
     [dict]:
 
 """
-import boto3
 import os
 from datetime import datetime
 import logging
+import boto3
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -19,31 +19,29 @@ dest_key = os.environ['dataset_folder']
 
 
 def incoming_data_mover(filedate):
-    
+
     s3_client = boto3.client("s3")
-    s3 = boto3.resource("s3")
-    bucket = s3.Bucket(bucket_name)
-    eventDate = datetime.strptime(filedate, "%Y-%m-%d")
-    dd, mm, yyyy = str(eventDate.day).zfill(2), str(eventDate.month).zfill(2), str(eventDate.year)
-    print(f"""{yyyy}-{mm}-{dd}""")
-    final_source_key = f"{source_key}/yyyy={yyyy}/mm={mm}/dd={dd}"
+    s3_resource = boto3.resource("s3")
+    bucket = s3_resource.Bucket(bucket_name)
+    event_date = datetime.strptime(filedate, "%Y-%m-%d")
+    day, month, yyyy = str(event_date.day).zfill(2), str(event_date.month).zfill(2), str(event_date.year)
+    print(f"""{yyyy}-{month}-{day}""")
+    final_source_key = f"{source_key}/yyyy={yyyy}/mm={month}/dd={day}"
     for bucket_object in bucket.objects.filter(Prefix=final_source_key):
         path, file = os.path.split(bucket_object.key)
         copy_source_object = {
             'Bucket': bucket_name, 'Key': f"""{path}/{file}"""
         }
-        if file == "":
-            continue
-        else:
+        if file != "":
             s3_client.copy_object(CopySource=copy_source_object, Bucket=bucket_name,
-                                  Key=f"""{dest_key}/yyyy={yyyy}/mm={mm}/dd={dd}/{file}""") 
+                                  Key=f"""{dest_key}/yyyy={yyyy}/mm={month}/dd={day}/{file}""") 
             s3_client.delete_object(Bucket=bucket_name, Key=f"""{path}/{file}""")
     return "success"
 
 
 def lambda_handler(event: dict, _context: dict) -> dict:
     """Main lambda handler for Incoming Data to S3 Transform Location Lambda."""
-    
+
     # In case a correct event is encountered -------------------------------------------
     if event:
         # Create template and extract values from event --------------------------------
@@ -62,5 +60,6 @@ def lambda_handler(event: dict, _context: dict) -> dict:
     else:
         logging.error("We couldn't find a suitable event. Exiting....")
         raise OSError("No event found")
+
     
     
